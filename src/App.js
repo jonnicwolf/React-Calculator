bc68b350806f2ab20eb706ebd231265280d5e633
Jon Narine <jonnicwolf@gmail.com>
2021-04-28 14:00:13 -0400

    commit before push

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
 import './App.css';
-
+import Results from './components/results.js'
+ 
 function App() {
 
-  rand = (mkx, min, ) => {
-    
+  const rand = ( max, min ) => {
+    return (
+      Math.floor( Math.random() * (max - min) + min)
+    )
   }
- 
+  console.log(rand(10, 1))
+
   return (
     <>
-      <div className='values'>      
-        <button>1</button>      
-        <button>2</button>      
-        <button>3</button>      
-        <button>4</button>      
-        <button>5</button>      
-        <button>6</button>      
-        <button>7</button>      
-        <button>8</button>      
-        <button>9</button>      
-        <button id='zero'>0</button>      
-        <button>.</button>      
-      </div>
-      <div className='operators'>
-        <button>÷</button>
-        <button>x</button>
-        <button>-</button>
-        <button>+</button>
-      </div>
-      <div>
-        <button
-          onClick={rand}  
-        >Rand</button>
-      </div>
+      <div className='calculator'>
+        <div>
+          <div className='clears'>
+            <button>AC</button>
+            <button>C</button>
+            <button>Del</button>
+            <button>(x)-1</button>
+          </div>
+          <div className='values'>      
+            <button>1</button>      
+            <button>2</button>      
+            <button>3</button>      
+            <button>4</button>      
+            <button>5</button>      
+            <button>6</button>      
+            <button>7</button>      
+            <button>8</button>      
+            <button>9</button>      
+            <button id='zero'>0</button>      
+            <button>.</button>      
+          </div>
+          <div className='operators'>
+            <button>÷</button>
+            <button>x</button>
+            <button>-</button>
+            <button>+</button>
+            </div>
+        </div>
+      
+          <Results />
+        </div>
     </>
   )
 }
--- src/components/Values.js
+++ src/components/Values.js
@@ -3,7 +3,61 @@ import React from 'react'
 function Values (props) {
     return (
       <form>
-        {props}
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+        <label htmlFor='1'>1</label>
+        <input type='button'
+               name='1'
+               />
+
+
+        <div className='values'>      
+            <button>1</button>      
+            <button>2</button>      
+            <button>3</button>      
+            <button>4</button>      
+            <button>5</button>      
+            <button>6</button>      
+            <button>7</button>      
+            <button>8</button>      
+            <button>9</button>      
+            <button id='zero'>0</button>      
+            <button>.</button>      
+          </div>
       </form>
     )
 }
