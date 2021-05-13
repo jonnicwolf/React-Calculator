3807b263fedc2f93a9fe7c7eec1e2878cc16208c
Colin Jaffe <balloonasaurus@gmail.com>
2021-05-13 17:57:11 -0400

    Adds teacher comments.

--- src/components/results.js
+++ src/components/results.js
@@ -5,6 +5,9 @@ export default function results(props) {
   const { input } = props;
   // this should be fine since the only inputs are numbers right?
   let output;
+  // A nested if/else statement can be challenging to follow.
+  // I'd recommend separate if statements.
+  // Or, even better, a helper function that returns a value.
   if (isNaN(input[input.length - 1])) {
     output = input;
     console.log('first if ran')
@@ -19,6 +22,8 @@ export default function results(props) {
     output = eval(input);
   }
 
+  // The extra space after a curly brace (and before a return) are good ideas.
+  // They make your code much more readable!
   return (
     <div className="results">
       <div id="input">{input}</div>
